bf1c1df0053bcd020bed70ca4720268e6ea67401
Nicolas Karagozian <nkaragozian@udesa.edu.ar>
2025-10-18 12:21:49 -0300

    paso 2

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
-# Directorio donde se guardarán los archivos
-FILES_DIRECTORY = "./files"
-
-# Asegurarse de que el directorio exista
-os.makedirs(FILES_DIRECTORY, exist_ok=True)
-
-
-@app.get("/files")
-async def list_files():
-    """
-    Este endpoint devuelve una lista de los archivos
-    disponibles en el servidor.
-    """
-    try:
-        files = os.listdir(FILES_DIRECTORY)
-        return {"files": files}
-    except Exception as e:
-        return {"error": str(e)}
+@app.get("/")
+async def prueba_root():
+    return {"message": "Accediste al endpoint de prueba"}
